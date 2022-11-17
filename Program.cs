3369364a2c43b0c08d58e440b921b05c32cc6241
David R. K. DeLoveh <ddeloveh@users.noreply.github.com>
2022-11-16 20:56:03 -0500

    Initial add

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+// See https://aka.ms/new-console-template for more information
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
+    {
+        configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+        //appsetting.json & appsettings.{Environment}.json are loaded by default
+        // so this just adds an optional override file you wouldn't commit to git
+        configurationBuilder.AddJsonFile("appsettings.local.json", optional: true);
+    })
+    .ConfigureLogging((hostBuilderContext, loggingBuilder) =>
+    {
+        loggingBuilder.AddConsole();
+    })
+    .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("A message being logged.");
+
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+//Example of a config retrieval
+var key1 = config.GetValue<string>("Key1", "Default for MISSING");
+logger.LogInformation($"Key1={key1}");
+
+var dbConnectionString = config.GetConnectionString("DB1");
+logger.LogInformation($"DB1={dbConnectionString}");
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ConsoleStarter
  Example starter console targeting net6.
+
+This adds example configurations for Development and Production with an optional "appsettings.local.json".  This is added as optional so it can override when developing, but not sent to git. Added last so values in this will always be used.
+
+Examples show getting a generic key as well as a connection string for a DB.
+
+Logging is being sent to the console.
+
+The launchSettings.json file includes the "DOTNET_ENVIRONMENT" to force it "DEVELOPMENT".
